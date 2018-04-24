54275404aa40daef5c131c45e73e4b5be3d897f6
Daniel Foster <danieljfoster87@gmail.com>
2018-04-24 15:22:26 -0400

    change list format

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,54 +19,66 @@ connection.connect(function(err) {
 });
 
 function purchase() {
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, results) {
-    if (err) throw err;
+  connection.query(
+    "SELECT item_id, product_name, price, stock_quantity FROM products",
+    function(err, results) {
+      if (err) throw err;
 
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "list",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(
-                results[i].item_id + " " + results[i].product_name + " $" + results[i].price + ".00"
-              );
-            }
-            return choiceArray;
+      console.log(
+        "\--------------------" +
+          "\n--------------------" +
+          "\nWelcome to Bamazon!" +
+          "\n--------------------" +
+          "\n--------------------" +
+          "\nHere is a list of available items:" +
+          "\n--------------------"
+      );
+
+
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i].item_id + " " + " " + results[i].product_name + " " + " $" + results[i].price + ".00");
+      }
+
+      console.log("--------------------")
+
+      inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "input",
+            message:
+              "What is the id of the item you would like to purchase?"
           },
-          message: "What item would you like to purchase?"
-        },
-        {
-          name: "item",
-          type: "input",
-          message: "How many would you like to purchase?"
-        }
-      ])
-      .then(function(answer) {
-        for (var i = 0; i < results.length; i++) {
-          var chosenItem = answer.choice.item_id;
-        }
+          {
+            name: "item",
+            type: "input",
+            message:
+              "How many would you like to purchase?"
+          }
+        ])
+        .then(function(answer) {
+          for (var i = 0; i < results.length; i++) {
+            var chosenItem = answer.choice.item_id;
+          }
 
-        if (chosenItem.stock_quantity >= parseInt(answer.item)) {
-          connection.query(
-            "UPDATE products SET stock_quantity = " +
-              newQuantity +
-              "WHERE item_id = " +
-              chosenItem,
+          if (choices.stock_quantity >= parseInt(answer.item)) {
+            connection.query(
+              "UPDATE products SET stock_quantity = " +
+                newQuantity +
+                "WHERE item_id = " +
+                chosenItem,
 
-            function(error) {
-              if (error) throw err;
-              console.log("Congratulations on your purchase!");
-              purchase();
-            }
-          );
-        } else {
-          console.log("Insufficient quantity!");
-          connection.end();
-        }
-      });
-  });
+              function(error) {
+                if (error) throw err;
+                console.log("Congratulations on your purchase!");
+                purchase();
+              }
+            );
+          } else {
+            console.log("Insufficient quantity!");
+            connection.end();
+          }
+        });
+    }
+  );
 }
-
